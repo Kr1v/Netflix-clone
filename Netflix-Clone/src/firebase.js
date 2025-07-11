3127ddb1e9ddb1224a930fa2dab6016e83980431
Kritika Mehta <162875055+Kr1v@users.noreply.github.com>
2025-07-11 06:43:01 +0530

    moved and renamed

---
+++ Netflix-Clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBY6uc4i6umiwlywIyr422lmxu5TSqWETg",
+  authDomain: "netflix-clone-app-9a653.firebaseapp.com",
+  projectId: "netflix-clone-app-9a653",
+  storageBucket: "netflix-clone-app-9a653.firebasestorage.app",
+  messagingSenderId: "724100288768",
+  appId: "1:724100288768:web:13ec71f893f33797b8a3e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
